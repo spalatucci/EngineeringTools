3f12fcdbe5d8c7b336472db198196223b6ec3230
Sam Palatucci <sam.palatucci@outlook.com>
2025-07-09 15:12:25 -0400

    commit

--- Webex/zTraining/Webex Calling for Government (FedRAMP) Bootcamp/Configs/dcloud/Randon_Num.ps1
+++
@@ -1,6 +0,0 @@
-﻿for($i=1;$i -le 40;$i++)
-{$num=Get-Random -Minimum 4112223333 -Maximum 9999999999
- $num = "$num"+","
- if ($i -eq 40) {$num = $num.Substring(0, $num.Length-1)}
- Add-Content -Path C:\Users\cholland\Desktop\DID_numbers.txt "$num"
- }
